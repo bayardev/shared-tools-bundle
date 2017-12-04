41990fa7d2067a4e15b07e0685dff561437fd264
Massimiliano Pasquesi <massimiliano.pasquesi@bayard-presse.com>
2017-12-04 15:33:15 +0100

    Improved BayardWebProcessor data

--- Logger/BayardWebProcessor.php
+++ Logger/BayardWebProcessor.php
@@ -15,14 +15,13 @@ class BayardWebProcessor extends WebProcessor
      * {@inheritdoc}
      * @param string|null kernel parameter kernel.root_dir
      */
-    public function __construct($serverData = null, array $extraFields = null, $kernel_root_dir = null)
+    public function __construct($serverData = null, array $extraFields = null, $kernel_root_dir = null, $requestStack = null)
     {
         $this->app_name = (null === $kernel_root_dir) ?
             "app" :
             basename(dirname($kernel_root_dir));
 
-        $this->addExtraField('client_ip', 'HTTP_CLIENT_IP');
-        $this->addExtraField('forwarded_for', 'HTTP_X_FORWARDED_FOR');
+        $this->requestStack = $requestStack;
 
         parent::__construct($serverData, $extraFields);
     }
@@ -30,13 +29,27 @@ class BayardWebProcessor extends WebProcessor
     /**
      * {@inheritdoc}
      */
-    public function __invoke(array $record) {
-
+    public function __invoke(array $record)
+    {
         if (!isset($this->serverData['REQUEST_URI'])) {
             $record['extra'] = ['server' => $this->app_name];
             return $record;
         }
 
-        return parent::__invoke($record);
+        if (  !array_key_exists('HTTP_CLIENT_IP', $this->serverData) && ($request = $this->requestStack->getCurrentRequest()) )
+        {
+            $this->addExtraField('client_ip', 'HTTP_CLIENT_IP');
+            $this->serverData['HTTP_CLIENT_IP'] = $request->getClientIp();
+
+        } else if (array_key_exists('HTTP_CLIENT_IP', $this->serverData)) {
+            $this->addExtraField('client_ip', 'HTTP_CLIENT_IP');
+        }
+
+        if (array_key_exists('HTTP_X_FORWARDED_FOR', $this->serverData)) {
+            $this->addExtraField('forwarded_for', 'HTTP_X_FORWARDED_FOR');
+        }
+
+        $record = parent::__invoke($record);
+        return $record;
     }
 }
